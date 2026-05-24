2463d8af0100e88e5f265874e9145921be8f44b9
andrewboinik <git@stackblitz.com>
2026-05-24 16:58:00 +0300

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      
+      {/* Экран 1 */}
+      <section className="page">
+        <h1>Добро пожаловать в SportUp</h1>
+      </section>
+
+      {/* Экран 2 */}
+      <section className="page about-section">
+        <h1>О Нас</h1>
+      </section>
+
+    </div>
+  );
+}
+
+export default Home;
